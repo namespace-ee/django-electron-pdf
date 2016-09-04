d19a35f0fd9b93b3b8618c8945d969bd146f0e63
Madis Väin <madis@namespace.ee>
2016-09-04 23:41:42 +0300

    required PyPi setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name='django-electron-pdf',
+    packages=['django-electron-pdf'],
+    version='0.1',
+    description='A Django wrapper to generate PDF from URL, HTML or Markdown files.',
+    author='Madis Väin',
+    author_email='madisvain@gmail.com',
+    url='https://github.com/namespace-ee/django-electron-pdf',
+    download_url='https://github.com/namespace-ee/django-electron-pdf/tarball/0.1',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
