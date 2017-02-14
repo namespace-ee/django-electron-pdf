ab5f07eed337915185ef95ec55fa1d2abcfbebfd
Meiko Udras <meikoudras@gmail.com>
2017-02-14 17:30:54 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='django-electron-pdf',
     packages=['electron_pdf'],
-    version='0.4',
+    version='0.4.1',
     description='A Django wrapper to generate PDF from URL, HTML or Markdown files.',
     author='Madis Väin',
     author_email='madisvain@gmail.com',
